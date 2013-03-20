3f11f2316bb0fbb7f00960ffbed2c9295f64b2f8
mmeyet1 <mmeyet1@students.towson.edu>
2013-03-20 18:22:08 -0400

    create helpful documentation pages and added links from the main page, continued to format proposal page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,12 @@ class HomeController < ApplicationController
   def index
     @users = User.all
   end
+  
+  def welcome
+    @users = User.all
+  end
+  
+  def after_sign_in_path_for(resource)
+    current_user_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails3BootstrapDeviseCancan::Application.routes.draw do
   resources :proposals
 
-
   authenticated :user do
-    root :to => 'home#index'
+    root :to => 'home#main'
   end
-  root :to => "home#index"
+  root :to => "home#main"
   devise_for :users
   resources :users
+  
+  match "tos", :to => "home#tos"
+  match "cp", :to => "home#cp"
+  match "primer", :to => "home#primer"
+  
+  get "home/main"
 end
